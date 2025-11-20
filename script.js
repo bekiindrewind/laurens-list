5b95a736de35903ccf0bf0f22cb0bbb8aff8d520
Brian French <snap.overst33r@gmail.com>
2025-11-20 18:34:10 -0500

    Add 'dying [family/relationship]' phrases to cancer terms
    - Add phrases: 'dying mother', 'dying father', 'dying brother', 'dying sister', 'dying friend'
    - Add phrases: 'dying uncle', 'dying aunt', 'dying grandmother', 'dying grandfather'
    - Add phrases: 'dying parent', 'dying parents', 'dying child', 'dying children'
    - Add phrases: 'dying spouse', 'dying husband', 'dying wife', 'dying partner'
    - Catches movies/books where family members are dying (often cancer-related)

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ const CANCER_TERMS = [
     'medical treatment', 'undergoes treatment', 'receives treatment',
     'hospital stay', 'hospitalization', 'hospitalized',
     'kids in her ward', 'kids in his ward', 'kids in their ward', 'children in her ward', 'children in his ward', 'children in their ward',
+    'dying mother', 'dying father', 'dying brother', 'dying sister', 'dying friend', 'dying uncle', 'dying aunt', 'dying grandmother', 'dying grandfather', 'dying parent', 'dying parents', 'dying child', 'dying children', 'dying spouse', 'dying husband', 'dying wife', 'dying partner',
     'sick with', 'illness strikes', 'disease affects',
     'coping with illness', 'coping with disease', 'living with illness', 'living with disease',
     'illness story', 'disease story', 'medical drama', 'illness drama',
